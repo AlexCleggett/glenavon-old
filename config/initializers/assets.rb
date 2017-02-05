36e56dd2f12a34fc4a4c800412d94a72c321ce4c
Alex Cleggett <ajcleggett@gmail.com>
2017-02-05 08:43:01 +0000

    app is now up to date

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,12 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( base.css )
+Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( vines.css )
+Rails.application.config.assets.precompile += %w( vine_order.js )
+Rails.application.config.assets.precompile += %w( print.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
