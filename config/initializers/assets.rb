5e7e1e68731ee6a5ecd74c3d2f076744d8bc6aa8
Alex Cleggett <ajcleggett@gmail.com>
2017-03-03 11:49:15 +0000

    adding cool stuff on index page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,6 @@ Rails.application.config.assets.precompile += %w( home.css )
 Rails.application.config.assets.precompile += %w( vines.css )
 Rails.application.config.assets.precompile += %w( vine_order.js )
 Rails.application.config.assets.precompile += %w( print.css )
-Rails.application.config.assets.precompile += %w( bootstrap.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
